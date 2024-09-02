f6f2749b3a3f41204d1ed6a1ce39124671a57b44
dennislee928 <55692615+dennislee928@users.noreply.github.com>
2024-09-02 21:14:22 +0800

    fix

--- front-page/plugins/ i18n.ts
+++ front-page/plugins/ i18n.ts
@@ -1,10 +1,11 @@
-import { defineNuxtPlugin } from "#app";
+import { defineNuxtPlugin } from "#imports"; // 修改此行
 import { createI18n } from "vue-i18n";
 // 以下語系自行添加
 import en from "../locales/en.json";
 import tw from "../locales/tw.json";
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin((nuxtApp: any) => {
+  // 修改此行
   const i18n = createI18n({
     locale: "en",
     messages: {
